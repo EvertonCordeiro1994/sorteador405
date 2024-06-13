49ee29a8a5338189a288fe72471a9014e9ae99a8
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2024-06-13 17:17:23 -0300

    inicio da correção do bug

--- script.js
+++ script.js
@@ -12,8 +12,24 @@ btn.addEventListener("click", function(){
     const sorteado = sorteio(0, (nomes.length - 1))
 
     console.log(nomes[sorteado])
+
+    numSort = nomes[sorteado];
+
+    const guardadoSet = localStorage.setItem("num", numSort);
+    const sort = localStorage.getItem("num");
+
+    if(sort === nomes){
+        const guardadoSet = localStorage.setItem("num", numSort);
+        const sort = localStorage.getItem("num");
+
+        document.querySelector("#tela").innerText = sort;
+
+    }else{
+        const sorteado = sorteio(0, (nomes.length - 1));
+        document.querySelector("#tela").innerText = sort;
+    }
+
     
-    document.querySelector("#tela").innerText = nomes[sorteado];
 
 
 });
